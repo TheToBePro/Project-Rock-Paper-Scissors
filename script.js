84416c6bd40804f7e27b00d1152f9483cd5b2431
TheToBePro <zoolcool839@gmail.com>
2023-03-31 06:51:55 -0700

    modified the way of putting the if statements

--- script.js
+++ script.js
@@ -3,40 +3,46 @@
 // make the computer one of the players where it select RANDOM choice
 // make the player input his chioce Rock Paper or scissors
 // 
+//make game choice
 const gameChoices = [
     "ROCK",
     "PAPER",
     "SCISSOR"
 ]
+
+//function to get computer choice 
 function getComputerChoice() {
     let chioce = gameChoices[Math.floor(Math.random() * 3)];
     return chioce;
 }
+
 let computerSelection = getComputerChoice();
+
+// getting player choice 
 let playerChoice = prompt("please input your choice: ");
 
+// loop until get right choice
 while(!gameChoices.includes(playerChoice.toUpperCase())){
      playerChoice = prompt("Wrong choice rock, paper, scissers: ");
 }
 
 let playerSelection = playerChoice.toUpperCase();
 
+//function for detect the winner
 function playRound(playerSelection, computerSelection) {
     let result;
     if(playerSelection === computerSelection){
          result = "it is tie";
-    }else if(playerSelection === "ROCK" && computerSelection === "SCISSOR") {
-        result = "player Win";
-    }else if(playerSelection === "PAPER" && computerSelection === "ROCK"){
-        result = "player Win";
-    }else if(playerSelection === "SCISSOR" && computerSelection === "PAPER"){
-        result = "player Win";
-    }else if(computerSelection === "ROCK" && playerSelection === "SCISSOR"){
-        result = "Computer Win";
-    }else if(computerSelection === "PAPER" && playerSelection === "ROCK"){
-        result = "Computer Win";
-    }else if(computerSelection === "SCISSOR" && playerSelection === "PAPER"){
-        result = "Computer Win";
+    }else if(playerSelection === "ROCK" && computerSelection === "SCISSOR"
+            || playerSelection === "PAPER" && computerSelection === "ROCK"
+            ||playerSelection === "SCISSOR" && computerSelection === "PAPER") {
+        
+                result = "player Win";
+    }else if(computerSelection === "ROCK" && playerSelection === "SCISSOR"
+            || computerSelection === "PAPER" && playerSelection === "ROCK"
+            || computerSelection === "SCISSOR" && playerSelection === "PAPER"){
+        
+                result = "Computer Win";
     }
     return result;
 }
